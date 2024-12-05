c68e727bf3904a7f36cb1cc6cbf57cfbb04c86d1
Fernando Muñoz <fernandofailed@gmail.com>
2024-12-05 15:16:17 -0300

    feature: se mejora el modelo aumentando los datos de entrada y los frames

--- exporting_model/constants.py
+++ exporting_model/constants.py
@@ -4,6 +4,8 @@ import os
 MODEL_FRAMES = 30
 
 ROOT_PATH = os.getcwd()
-MODEL_FOLDER_PATH = os.path.join(ROOT_PATH, "models")
-MODEL_PATH = os.path.join(MODEL_FOLDER_PATH, f"actions_{MODEL_FRAMES}.keras")
-MODEL_PATH2 = os.path.join(MODEL_FOLDER_PATH, f"actions_{MODEL_FRAMES}.h5")
+MAIN_MODEL_FOLDER_PATH = os.path.join(ROOT_PATH, "models/")
+MODEL_FOLDER_PATH = os.path.join(ROOT_PATH, "models/webModel")
+TFLITE_MODEL_PATH = os.path.join(os.path.dirname(__file__), 'models', 'liteModel', 'model.tflite')
+MODEL_PATH = os.path.join(MAIN_MODEL_FOLDER_PATH, f"actions_{MODEL_FRAMES}.keras")
+MODEL_PATH2 = os.path.join(MAIN_MODEL_FOLDER_PATH, f"actions_{MODEL_FRAMES}.h5")
